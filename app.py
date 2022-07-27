f80a9b5f802d23d6cbaa1677e7c2fd0f5572ca2f
andreneox <andreneox@gmail.com>
2022-07-27 00:34:00 +0000

    Feat: HTML e CSS e conexao entre paginas

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask ("hello")
 
-@app.route ("/")
-@app.route("/hello")
-def hello(): 
-    return "Hello World"
+posts = [
+        {
+            "title" : "Meu primeiro post",
+            "body" : "Aqui e o texto do post",
+            "author" : "Andre",
+            "created" : datetime(2022, 7, 25)
+        },
+         {
+            "title" : "Meu segundo post",
+            "body" : "Aqui e o texto do post",
+            "author" : "Jojo",
+            "created" : datetime(2022, 7, 26)
+        },
+]
 
-@app.route ("/meucontato")
-def meuContato():
-    return render_template('index.html')
+@app.route ("/")
+def index(): 
+    return render_template ("index.html", posts=posts)
+
+@app.route ("/login")
+def login(): 
+    return render_template ("login.html", posts=posts)
